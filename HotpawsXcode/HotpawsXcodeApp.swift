dd98c5fb472ab2b290880be1c19217d73100de6a
Teo <214712079+listeomin@users.noreply.github.com>
2025-12-05 15:25:16 +0300

    Initial Commit

---
+++ HotpawsXcode/HotpawsXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotpawsXcodeApp.swift
+//  HotpawsXcode
+//
+//  Created by Teo on 2025-12-05.
+//
+
+import SwiftUI
+
+@main
+struct HotpawsXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
